96b1d673fce613ef164193cd4f9969abc1bb9957
Kaooot <37877491+Kaooot@users.noreply.github.com>
2026-05-08 20:38:52 +0200

    Fix forceClientCloseInventory exception again

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -4759,8 +4759,9 @@ public class Player extends EntityHuman implements CommandSender, ChunkLoader, I
      */
     @UnintendedClientBehaviour
     public void forceClientCloseInventory() {
-        setDataProperty(ActorDataTypes.PLAYER_FLAGS, getDataProperty(ActorDataTypes.PLAYER_FLAGS, (byte) 0) | (byte) 0x2);
-        getLevel().getScheduler().scheduleDelayedTask(() -> setDataProperty(ActorDataTypes.PLAYER_FLAGS, getDataProperty(ActorDataTypes.PLAYER_FLAGS, (byte) 0) & (byte) 0x1), 2);
+        final byte value = getDataProperty(ActorDataTypes.PLAYER_FLAGS, (byte) 0);
+        setDataProperty(ActorDataTypes.PLAYER_FLAGS, ((Integer) (value | (byte) 0x2)).byteValue());
+        getLevel().getScheduler().scheduleDelayedTask(() -> setDataProperty(ActorDataTypes.PLAYER_FLAGS, ((Integer) (getDataProperty(ActorDataTypes.PLAYER_FLAGS, (byte) 0) & (byte) 0x1)).byteValue()), 2);
     }
 
     /**
